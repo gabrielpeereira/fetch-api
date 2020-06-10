ecbe03664347f49af60a763d55d4225f2f38ddfa
Gabriel Pereira <gabriel.developer@outlook.com>
2020-06-10 15:48:27 -0300

    cadastrao de clientes com method post

--- api/cliente.js
+++ api/cliente.js
@@ -1,9 +1,26 @@
-listarClientes = () => {
+const listarClientes = () => {
     return fetch('http://localhost:4000/clientes')
-    .then(response => {
-        return response.json()
+        .then(response => {
+            return response.json()
+        })
+        .then(json => {
+            return json;
+        })
+}
+
+const cadastrarClientes = (nome, cpf) => {
+    const Json = JSON.stringify({
+       nome: nome,
+       cpf: nome  
     })
-    .then(json => {
-        return json;
+    return fetch('http://localhost:4000/clientes/cliente', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: Json
+    })
+    .then(response => {
+        return response.body
     })
 }
